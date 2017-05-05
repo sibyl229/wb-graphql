b8c56204212eb78a3da2ae875089e4f0306588ed
Sibyl <sibyl@wormbase.org>
2017-05-05 10:38:39 -0400

    redirect to index.html (graphiql ide) based on content-type

--- src/wb_graphql/handler.clj
+++ src/wb_graphql/handler.clj
@@ -16,9 +16,11 @@
   (routes
    (GET "/" [] "<h1>Hello World</h1>")
    (GET "/graphql" [schema query variables :as request]
-        (println "GET query: " query)
-        (response/response
-         (graphql-executor query variables)))
+        (if (= (:content-type request) "application/json")
+          (do (println "GET query: " query)
+              (response/response
+               (graphql-executor query variables)))
+          (response/redirect "/index.html" 301)))
    (POST "/graphql" [schema query variables :as request]
          (println "POST query: " query)
          ;; (println "Post variables: " (json/parse-string variables))
