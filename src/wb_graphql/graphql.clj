76a58b5314dd278c86061924eaa86db071f22a9c
Sibyl <sibyl@wormbase.org>
2017-05-05 15:55:15 -0400

    reduce footprint of (d/db datomic-conn), try pass in db as parameter

--- src/wb_graphql/graphql.clj
+++ src/wb_graphql/graphql.clj
@@ -230,15 +230,13 @@ type %sConnection {
            [?e :pace/use-ns ?inf]]
          db))
 
-(defn x [ident]
-  (let [db (d/db datomic-conn)]
-    (d/q '[:find (pull ?e [*])
-           :in $ ?ident
-           :where
-           [?e :db/ident ?ident]
-           (not [_ :db.install/attribute ?e])
-           ]
-         db ident)))
+(defn x [db ident]
+  (d/q '[:find (pull ?e [*])
+         :in $ ?ident
+         :where
+         [?e :db/ident ?ident]
+         (not [_ :db.install/attribute ?e])]
+       db ident))
 
 (defn generate-type-schema [db]
   (let [ts (type-names db)]
@@ -266,8 +264,8 @@ type Query {
 ")))
 
 
-(defn starter-resolver-fn [type-name field-name]
-  (let [db (d/db datomic-conn)]
+(defn starter-resolver-factory [db]
+  (fn [type-name field-name]
     (match/match
      [type-name field-name]
      ["Query" "getGenesByNames"] (fn [context parent args]
@@ -331,7 +329,11 @@ type Query {
   (let [validated-schema (load-validated-schema)
         context nil]
     (fn [query variables]
-      (executor/execute context validated-schema starter-resolver-fn query variables))))
+      (executor/execute context
+                        validated-schema
+                        (starter-resolver-factory db)
+                        query
+                        variables))))
 
 (defn merge-schema [& schema-parts]
   (->> (flatten schema-parts)
